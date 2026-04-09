efc2e82a298259228e9b3327ad7c5ca227c2cfb6
caelondev <227796969+caelondev@users.noreply.github.com>
2026-04-09 11:02:31 +0800

    feat: implemented TXDatabaseManager

--- src/core/database/TXDatabaseManager.ts
+++ src/core/database/TXDatabaseManager.ts
@@ -1,1 +1,33 @@
-export default class TXDatabaseManager {}
+import mongoose, { Schema, Model } from "mongoose";
+import TXLogger from "../logger/TXLogger.js";
+import { DebugLevel } from "../../types/TXDebugLevel.js";
+
+export default class TXDatabaseManager {
+  private databaseKey: string;
+  private logger: TXLogger;
+  private models: Map<string, Model<any>> = new Map();
+
+  constructor(databaseKey: string, logger: TXLogger) {
+    this.databaseKey = databaseKey;
+    this.logger = logger;
+  }
+
+  public async connect() {
+    if (mongoose.connection.readyState === 1) return;
+
+    this.logger.log("Connecting to MongoDB", DebugLevel.Info);
+    await mongoose.connect(this.databaseKey);
+    this.logger.log("Connected to MongoDB", DebugLevel.Ok);
+  }
+
+  public createModel<T>(name: string, schema: Schema<T>): Model<T> {
+    if (this.models.has(name)) {
+      return this.models.get(name) as Model<T>;
+    }
+
+    const model = mongoose.model<T>(name, schema);
+    this.models.set(name, model);
+
+    return model;
+  }
+}
