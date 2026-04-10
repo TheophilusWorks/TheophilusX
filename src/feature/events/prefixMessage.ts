75ea951f85f391e17481a67bc0581c19d0389895
caelondev <227796969+caelondev@users.noreply.github.com>
2026-04-10 23:53:38 +0800

    feat: added prefix replier just in case someone asked for the prefix

---
+++ src/feature/events/prefixMessage.ts
@@ -0,0 +1,40 @@
+import { TXPlatform } from "../../core/context/TXContext.js";
+import TXEventBuilder from "../../core/event/TXEventBuilder.js";
+import instance from "../../instance.js";
+
+const MESSAGED_GROUPS = new Map<string, number>();
+
+export default new TXEventBuilder("messageCreate", async (ctx, adapter) => {
+  if (ctx.author.isSelf) return;
+  if (!ctx.content.includes('prefix')) return;
+
+  let key = groupKey(ctx.platform, ctx.serverId);
+  let now = Date.now();
+
+  if ((MESSAGED_GROUPS.get(key) ?? 0) > now) return;
+
+  await adapter.reply(
+    ctx,
+    `
+‗   ↳ ❝ [ Prefix ] ¡! ❞
+ೃ⁀➷ Here  are my prefixes:
+         ◇─◇───◇─◇
+
+╭┈ info : ̗̀➛
+${instance.prefixes.map((v, i) => `┊ ${i + 1}: "${v}"`).join("\n")}
+╰─────────┈➤
+
+𓆩⟡𓆪 Type \`%help\` to see all available commands!
+`.trim(),
+  );
+
+  let cd = now + 60_000; // 1 min
+  MESSAGED_GROUPS.set(key, cd);
+  setTimeout(() => {
+    MESSAGED_GROUPS.delete(key);
+  }, 60_000);
+});
+
+function groupKey(platform: TXPlatform, serverId: string) {
+  return `${platform}-${serverId}`;
+}
