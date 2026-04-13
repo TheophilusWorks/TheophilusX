c80935cf50c17d30bfcc5fdffb44fee018f3a1ec
caelondev <227796969+caelondev@users.noreply.github.com>
2026-04-13 20:47:00 +0800

    rework: added a way to inspect other user's balance

--- src/feature/commands/economy/balance.ts
+++ src/feature/commands/economy/balance.ts
@@ -1,7 +1,11 @@
 import TXCommand from "../../../core/command/TXCommand.js";
-import Users from "../../../core/database/model/Users.js";
-import { queryUser } from "../../../core/database/model/Users.js";
+import { TXPlatform } from "../../../core/context/TXContext.js";
+import Users, {
+  queryUser,
+  initializeUserEconomy,
+} from "../../../core/database/model/Users.js";
 import { mention, text } from "../../../core/message/TXMessageBuilder.js";
+import { TXMessagePart } from "../../../core/message/TXMessagePart.js";
 
 export default new TXCommand({
   name: "balance",
@@ -13,22 +17,62 @@ export default new TXCommand({
   minimumGroupedArguments: 0,
   minimumMentions: 0,
   execute: async ({ adapter, context }) => {
-    let { platform } = context;
-    let { id, displayName } =
-      context.mentions.length != 0 ? context.mentions[0] : context.author;
-    let query = queryUser(platform, id);
-    let user = await Users.findOne(query);
-    if (!user) user = new Users(query);
+    let targetUser =
+      context.mentions.length !== 0 ? context.mentions[0] : context.author;
 
-    adapter.reply(context, {
-      parts: [
-        text("⋆˚꩜｡ "),
-        mention(id, displayName),
-        text(`'s Balance`),
-        text("\n﹌﹌﹌﹌﹌﹌﹌﹌﹌\n"),
-        text(`⛃⛂ Coins: ${user.economy?.coins}\n`),
-        text(`🏦💸 Bank balance: ${user.economy?.bankBalance}\n`),
-      ],
-    });
+    let parts: TXMessagePart[] = [];
+
+    if (targetUser.id == context.author.id) {
+      parts = await inspectSelf(context.author.id, context.platform);
+    } else {
+      parts = await inspectUser(
+        targetUser.id,
+        targetUser.displayName,
+        context.platform,
+      );
+    }
+
+    adapter.reply(context, { parts });
   },
 });
+
+async function inspectSelf(target: string, platform: TXPlatform) {
+  let user = await Users.findOneAndUpdate(
+    queryUser(platform, target),
+    {
+      $setOnInsert: { economy: initializeUserEconomy() },
+    },
+    { upsert: true, returnDocument: "after" },
+  );
+
+  return [
+    text("⋆˚꩜｡ "),
+    text(`Your Balance`),
+    text("\n﹌﹌﹌﹌﹌﹌﹌﹌﹌\n"),
+    text(`⛃⛂ Coins: ${user.economy?.coins}\n`),
+    text(`🏦💸 Bank balance: ${user.economy?.bankBalance}\n`),
+  ];
+}
+
+async function inspectUser(
+  target: string,
+  displayName: string,
+  platform: TXPlatform,
+) {
+  let user = await Users.findOneAndUpdate(
+    queryUser(platform, target),
+    {
+      $setOnInsert: { economy: initializeUserEconomy() },
+    },
+    { upsert: true, returnDocument: "after" },
+  );
+
+  return [
+    text("⋆˚꩜｡ "),
+    mention(target, displayName),
+    text(`'s Balance`),
+    text("\n﹌﹌﹌﹌﹌﹌﹌﹌﹌\n"),
+    text(`⛃⛂ Coins: ${user.economy?.coins}\n`),
+    text(`🏦💸 Bank balance: ${user.economy?.bankBalance}\n`),
+  ];
+}
