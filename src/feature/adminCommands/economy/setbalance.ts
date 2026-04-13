c4381fdd808ab38ef103611c06e0316bc961841b
caelondev <227796969+caelondev@users.noreply.github.com>
2026-04-13 20:33:53 +0800

    rework: `>setbalance` admin command only works as a one-liner now

--- src/feature/adminCommands/economy/setbalance.ts
+++ src/feature/adminCommands/economy/setbalance.ts
@@ -1,16 +1,80 @@
 import TXCommand from "../../../core/command/TXCommand.js";
-import Users, { queryUser } from "../../../core/database/model/Users.js";
+import Users, {
+  initializeUserEconomy,
+  queryUser,
+} from "../../../core/database/model/Users.js";
 import { mention, text } from "../../../core/message/TXMessageBuilder.js";
+import { TXIAuthor, TXPlatform } from "../../../core/context/TXContext.js";
+import { TXMessagePart } from "../../../core/message/TXMessagePart.js";
 
 export default new TXCommand({
   name: "setbalance",
   description: "Shows your balance",
-  usage: "setbalance <user> <coin | bank> <amount>",
+  usage: "setbalance (user1, user2, ...) <coins | bank> <amount>",
   minimumArguments: 2,
   minimumMentions: 1,
   aliases: ["setbal", "sb"],
   cooldown: 5_000,
   minimumGroupedArguments: 0,
-  execute: async ({ adapter, context }) => {
+  execute: async ({ adapter, context, args }) => {
+    let targets = context.mentions[0] ? context.mentions : [context.author];
+    let targetStorage = args[0].toLowerCase();
+    let amount = parseFloat(args[1]);
+
+    if (!["coins", "bank"].includes(targetStorage)) {
+      await adapter.reply(
+        context,
+        "Invalid storage. Please pick either 'coins' or 'bank'",
+      );
+      return;
+    }
+
+    console.log(targetStorage);
+
+    if (isNaN(amount) || amount < 0) {
+      await adapter.reply(
+        context,
+        "Invalid amount. Please enter a non-negative number",
+      );
+      return;
+    }
+
+    let parts: TXMessagePart[] = [];
+    for (const user of targets) {
+      await setUserStorageTo(user, context.platform, targetStorage, amount);
+      parts.push(mention(user.id, user.displayName));
+      parts.push(text(", ")); // seperator
+    }
+
+    // pop out the last guaranteed comma
+    parts.pop();
+
+    await adapter.reply(context, {
+      parts: [
+        text("Successfully set "),
+        ...parts,
+        text(`'s ${targetStorage} to ${amount}`),
+      ],
+    });
   },
 });
+
+async function setUserStorageTo(
+  user: TXIAuthor,
+  platform: TXPlatform,
+  storage: string,
+  amount: number,
+) {
+  storage = storage == "bank" ? "bankBalance" : storage;
+
+  await Users.findOneAndUpdate(
+    queryUser(platform, user.id),
+    {
+      $setOnInsert: { economy: initializeUserEconomy() },
+    },
+    { upsert: true },
+  );
+  await Users.findOneAndUpdate(queryUser(platform, user.id), {
+    $set: { [`economy.${storage}`]: amount },
+  });
+}
