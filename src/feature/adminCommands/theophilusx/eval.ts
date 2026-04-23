872a5fc60a89de264dfb90dd5df21058d4b423d9
caelondev <227796969+caelondev@users.noreply.github.com>
2026-04-23 16:55:35 +0800

    feat: added admin command eval

---
+++ src/feature/adminCommands/theophilusx/eval.ts
@@ -0,0 +1,15 @@
+import TXCommand from "../../../core/command/TXCommand.js";
+
+export default new TXCommand({
+  name: "eval",
+  description: "evalutes JavaScript code and returns the result",
+  usage: "eval <code>",
+  minimumArguments: 1,
+  minimumGroupedArguments: 0,
+  cooldown: 5_000,
+  minimumMentions: 0,
+  execute: async (ctx, { adapter }) => {
+    let value = eval(ctx.content);
+    await adapter.reply(ctx, `return value: ${value}`);
+  },
+});
