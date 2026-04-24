697d28d39f0cecc1324349ddde13464646c40efd
caelondev <227796969+caelondev@users.noreply.github.com>
2026-04-24 14:25:23 +0800

    chore: adjusted queue time

--- src/core/utils/TXMessageQueue.ts
+++ src/core/utils/TXMessageQueue.ts
@@ -14,8 +14,8 @@ export default class TXMessageQueue {
   private running: boolean;
 
   constructor(options: TXMessageQueueOptions = {}) {
-    this.minDelayMs = options.minDelayMs ?? 800;
-    this.maxDelayMs = options.maxDelayMs ?? 1500;
+    this.minDelayMs = options.minDelayMs ?? 1000;
+    this.maxDelayMs = options.maxDelayMs ?? 2000;
     this.queue = [];
     this.running = false;
   }
